43da92011956c2fa796a985787ac56354d0572b7
Nakatai <nakatai0322@gmail.com>
2021-10-02 14:22:32 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ $(function () {
     $('a[href^="#"]').on("click", function () {var href = $(this).attr("href");
       var target = $(href == "#" || href == "" ? "html" : href);
         var position = target.offset().top - navHeight;
-          $("html, body").animate({ scrollTop: position, }, 300, "swing");return false;});
+          $("html, body").animate({ scrollTop: position, }, 300, "swing");
+            return false;});
 
   //ページトップ
   $("#js-page-top").on("click", function () {$("body,html").animate({ scrollTop: 0, }, 300);
