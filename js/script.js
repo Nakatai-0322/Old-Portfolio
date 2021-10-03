f61255fe051929a7c51d9a88b20a7b01e21bf80b
Nakatai <nakatai0322@gmail.com>
2021-10-03 19:17:38 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function () {
 
   //ページ内スクロール]
   var navHeight = $(".header").outerHeight();
-    $('a[href^="#"]').on("click", function () {var href = $(this).attr("href");
+    $("a[href^="#"]").on("click", function () {var href = $(this).attr("href");
       var target = $(href == "#" || href == "" ? "html" : href);
         var position = target.offset().top - navHeight;
           $("html, body").animate({ scrollTop: position, }, 300, "swing");
