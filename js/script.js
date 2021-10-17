0a4271d920a9354013e6dbdc27402995d0019c5a
Nakatai-0322 <nakatai0322@gmail.com>
2021-10-17 09:13:44 +0900

    Update script.js
    var→const

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(function () {
 
   //ページ内スクロール
-  var navHeight = $(".header").outerHeight();
+  const navHeight = $(".header").outerHeight();
 
   $('a[href^="#"]').on("click", function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - navHeight;
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? "html" : href);
+    const position = target.offset().top - navHeight;
     $("html, body").animate({ scrollTop: position, }, 300, "swing");
     return false;
   });
